39d8d3c4469c77e3d3e967a22ec77102016700af
antonchepelev <anton.chepelev@gmail.com>
2024-09-17 18:38:41 -0400

    task 4 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int myNumber = 8;
+
+        int evenOrOdd = myNumber % 2;
+
+        System.out.println("my number is "+ myNumber+ ".  "+myNumber +
+                " modulus 2 is "+evenOrOdd+ " which means it is an even number");
     }
 }
